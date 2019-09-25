309060de588cc95742ab34e7cc8e1ed77b00e555
Eric <ericnavar@ufl.edu>
2019-09-25 16:06:02 -0400

    Create TextFileHelper class - include method readDirectory    - rename read_directory to readDirectory    - include bool parameter "extention" that if true includes the extention in the file names    - Remove functionality to print files, place it in printDirectory() - combineFiles() combines the contents of the text files in the directory, used to be main method - main method is now just a menu that lets you either print the directory or combine the files - include exe file

--- Combine.cpp
+++ Combine.cpp
@@ -6,40 +6,51 @@
 #include <windows.h>
 using namespace std;
 
-void read_directory(const string& name, vector<string>& fileNames)
+struct TextFileHelper
 {
-	ofstream outfile;
-	outfile.open("file_names.txt");
-	string pattern(name);
+	vector<string> directory;
+	string folderName;
+	void readDirectory(bool extension);
+	void printDirectory();
+	void combineFiles();
+	string removeExtention(string fileName);
+};
+
+void TextFileHelper::readDirectory(bool extention)
+{
+	string pattern(folderName);
 	pattern.append("\\*");
 	WIN32_FIND_DATA data;
 	HANDLE hFind;
 	if ((hFind = FindFirstFile(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE) {
 		do {
 			string s = data.cFileName;
-			fileNames.push_back(s);
-			outfile << s << endl;
-		} while (FindNextFile(hFind, &data)!=0);
+			if (extention) s = removeExtention(s);
+			directory.push_back(s);
+		} while (FindNextFile(hFind, &data) != 0);
 		FindClose(hFind);
 	}
-	outfile.close();
+	directory.erase(directory.begin(), directory.begin() + 2);
 }
 
-int main()
+void TextFileHelper::printDirectory()
 {
-	string folderName;
-	cin >> folderName;
+	ofstream outfile;
+	outfile.open("file_names.txt");
+	for (string s : directory)
+		outfile << s << endl;
+}
+
+void TextFileHelper::combineFiles()
+{
 	ofstream outfile;
 	outfile.open("combined_text.txt");
-	vector<string> fileNames;
-	read_directory(folderName + "/", fileNames);
-	fileNames.erase(fileNames.begin(), fileNames.begin() + 2);
 
 	if (!outfile.is_open())
 		cout << "outfile is not open." << endl;
 
 	ifstream infile;
-	for (string s : fileNames)
+	for (string s : directory)
 	{
 		string line;
 		infile.open(folderName + "/" + s);
@@ -49,6 +60,49 @@ int main()
 		infile.close();
 	}
 	outfile.close();
+}
+
+string TextFileHelper::removeExtention(string fileName)
+{
+	return fileName.substr(0, fileName.find("."));
+}
+
+int main()
+{
+	cout << "1: printDirectory" << endl;
+	cout << "2: combineFiles" << endl;
+	cout << "0: Exit" << endl;
+	bool extention;
+	string folderName;
+	int option = 1;
+	TextFileHelper t;
+	while (option != 0)
+	{
+		cin >> option;
+		switch (option)
+		{
+		case 0:
+			break;
+		case 1:
+			cout << "Folder name: ";
+			cin >> folderName;
+			cout << "include extention? 0:no 1:yes ";
+			cin >> extention;
+			cout << "debug: extention: " << extention;
+			if (t.directory.size() == 0) t.readDirectory(extention);
+			t.printDirectory();
+			break;
+		case 2:
+			cout << "Folder name: ";
+			cin >> folderName;
+			cout << "include extention? 0:no 1:yes ";
+			cin >> extention;
+			cout << "debug: extention: " << extention;
+			if (t.directory.size() == 0) t.readDirectory(extention);
+			t.combineFiles();
+			break;
+		}
+	}
 
 	return 0;
 }
